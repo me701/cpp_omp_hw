051c91780567797bb6921045d01ccd7bc9824080
Jeremy Roberts <jaroberts@ksu.edu>
2019-12-04 08:17:38 -0600

    Exercise 1 added.

---
+++ hello.cc
@@ -0,0 +1,20 @@
+// Exercise 1 
+// Print "hello from thread X of Y".
+#include <omp.h>
+#include <cstdio>
+
+
+
+int main(int argc, char* argv[])
+{
+
+
+{
+  int thread;
+  int num_threads;
+
+  printf("Hello from %i of %i \n", thread, num_threads);
+}
+
+  return 0;
+}
