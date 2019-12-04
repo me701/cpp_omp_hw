cb8c2f4e527285861685f70488a55cf0d2b03c7e
Jeremy Roberts <jaroberts@ksu.edu>
2019-12-04 08:26:36 -0600

    Exercise 3

---
+++ vector_add_2.cc
@@ -0,0 +1,32 @@
+// Exercise 3
+// Modify the vector addition from last time to use
+// an omp for directive.
+
+#include <omp.h>
+#include <cstdio>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+  
+  long int n = 10000000;
+  std::vector<double> a(n, 1.0);
+  std::vector<double> b(n, 2.0);
+  std::vector<double> c(n, 0.0);
+
+  // initialize timer
+  double et = omp_get_wtime();
+
+
+  for (long int i = 0; i < n; ++i)
+      c[i] = a[i] + b[i];
+
+  // finalize time
+  et = omp_get_wtime() - et;
+
+  printf("c[ 0]=%3.1f\n", c[0]);
+  printf("c[-1]=%3.1f\n", c[n-1]);
+  printf("time = %8.4f\n", et);
+
+  return 0;
+}
